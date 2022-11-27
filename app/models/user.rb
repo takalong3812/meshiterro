a42ad2be6d138393e790f0e14cb40b9d516045d3
EC2 Default User <ec2-user@ip-172-31-6-235.ap-northeast-1.compute.internal>
2022-11-27 09:42:17 +0000

    session22 ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_one_attached :profile_image
-  has_many :post_image, dependent: :destroy
+  has_many :post_images, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
